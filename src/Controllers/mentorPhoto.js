262f35695382e5f6c01e582fcea0999f6f740ee0
Thiago Cortez <thiago.cortez81@gmail.com>
2019-07-18 01:15:14 -0300

    Ajuste na rota de requisição da foto de perfil do user no github e remoção do yarn.lock (não deve ser comittado, pode trazer erros no yarn install)

--- src/Controllers/mentorPhoto.js
+++ src/Controllers/mentorPhoto.js
@@ -3,11 +3,11 @@ const fetch = require('node-fetch');
 module.exports = {
     async fetchPhoto(req,res){
         let { mentor } = req.params
-        const url = `http://api.github.com/users/${mentor}` 
+        const url = `https://github.com/${mentor}.png`
         const result = await fetch(url, {
             method: 'GET'
-        }).
-        then(response => response.json())
-        res.send(result.avatar_url)
+        });
+
+        res.send(result.url);
     }
 };
